0a5445e3f91971f729e8bbbeeaf306d323885f21
egidiotorcasio06-prog <egidiotorcasio06@gmail.com>
2025-10-29 12:42:35 +0100

    Update models.py

--- libri/models.py
+++ libri/models.py
@@ -5,6 +5,20 @@ class Libro(models.Model):
     autore = models.CharField(max_length=100)
     anno_pubblicazione = models.IntegerField()
     descrizione = models.TextField()
+    prezzo = models.DecimalField(
+        max_digits=6,      # Massimo 6 cifre totali
+        decimal_places=2,  # 2 cifre dopo la virgola (es: 9999.99)
+        verbose_name="Prezzo (€)",
+        help_text="Prezzo del libro in euro",
+        null=True,
+        blank=True
+    )
+    data_acquisto = models.DateField(
+        verbose_name="Data di acquisto",
+        help_text="Quando è stato acquistato il libro",
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.titolo
