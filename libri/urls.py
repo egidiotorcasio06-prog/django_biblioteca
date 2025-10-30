7a0aacb0d11971556b670da99d7a7bf134a06165
E T <egidiotorcasio06@gmail.com>
2025-10-30 10:32:28 +0100

    feat: aggiungi campi prezzo e data_acquisto al Model Libro e aggiornato il Form e il template di visualizzazione

--- libri/urls.py
+++ libri/urls.py
@@ -4,8 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.lista_libri, name='lista_libri'),
     path('aggiungi/', views.aggiungi_libro, name='aggiungi_libro'),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 0a5445e3f91971f729e8bbbeeaf306d323885f21
